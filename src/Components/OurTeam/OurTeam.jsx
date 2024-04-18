392b2e028ab8b47bb94a8a827525b2f9fb183520
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2024-04-19 00:03:26 +0530

    image size reduced

--- src/Components/OurTeam/OurTeam.jsx
+++ src/Components/OurTeam/OurTeam.jsx
@@ -63,6 +63,8 @@ const OurTeam = () => {
                     }`}
                     alt={data.Name}
                     loading="lazy"
+                    width={200}
+                    height={200}
                   />
                 )}
               </Image>
