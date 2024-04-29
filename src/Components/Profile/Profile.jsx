bc0f3b080f0bc6cd4a9e5327c5e54d447094ddf6
ByteZO <coder.gny@gmail.com>
2024-04-30 01:51:20 +0530

    Update padding and font-size in Profile.styled.js

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -5,8 +5,6 @@ import {
   MainInfo,
   CardsSection,
 } from "./Profile.styled";
-import axios from "axios";
-import { gapi } from "gapi-script";
 
 const GID = localStorage.getItem("user");
 const domainTitleStyle = {
@@ -38,21 +36,48 @@ const Profile = () => {
   const [testUserData, setTestUserData] = useState(null);
   const backendRequest = async () => {
     try {
-      const GIDFetch = await fetch(
-
-
-        `http://localhost:6001/megatronix/paridhi/user/profile/${GID}`
-
-      )
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setTestUserData(data);
-          return data;
-        });
-      console.log(GIDFetch);
+      // const GIDFetch = await fetch(
+      //   `http://localhost:6001/megatronix/paridhi/user/profile/${GID}`
+      // )
+      //   .then((res) => {
+      //     return res.json();
+      //   })
+      //   .then((data) => {
+      //     console.log(data);
+      //     setTestUserData(data);
+      //     return data;
+      //   });
+      // console.log(GIDFetch);
+      setTestUserData({
+        id: 1,
+        name: "Atul Kumar Singh",
+        college: "Techno",
+        year: "2",
+        department: "CSE",
+        roll: "123",
+        email: "cocatul11@gmail.com",
+        phoneNumber: "9123766255",
+        gid: "paridhi2000021020521024",
+        megaArchTid: "paridhi12002105202024",
+        setuBandhanTid: "paridhi12002105202024",
+        codezenTid: "paridhi12002105202024",
+        codeQuestTid: "paridhi12002105202024",
+        webMindsTid: null,
+        electriQuestTid: "paridhi12002105202024",
+        electrical2Tid: null,
+        bgmiLanTid: "paridhi12002105202024",
+        valorantLanTid: "paridhi12002105202024",
+        pesLanTid: "paridhi12002105202024",
+        bingeQuizTid: "paridhi12002105202024",
+        tableTennisTid: "paridhi12002105202024",
+        carromTid: "paridhi12002105202024",
+        lineTrekkerTid: "paridhi12002105202024",
+        triathlonTid: "paridhi12002105202024",
+        roboKlassikerTid: "paridhi12002105202024",
+        roboWar8kgTid: "paridhi12002105202024",
+        roboWar15kgTid: "paridhi12002105202024",
+        trackoteasureTid: "paridhi12002105202024",
+      });
     } catch (error) {
       console.error("Error fetching data: >>>>", error);
     }
@@ -64,7 +89,6 @@ const Profile = () => {
 
   console.log(testUserData);
   const {
-    id,
     name,
     college,
     year,
@@ -127,6 +151,23 @@ const Profile = () => {
     "Table Tennis TID": tableTennisTid,
     "Binge Quiz TID": bingeQuizTid,
   };
+
+  function cardSectionhandle() {
+    const domainArr = [
+      domainCivil,
+      domainCoding,
+      domainElectrical,
+      domainGaming,
+      domainGenral,
+      domainRobotics,
+    ];
+    const result = domainArr.map((domaine) => {
+      if (showCardHandler(domaine)) return
+    });
+    console.log(result);
+  }
+  cardSectionhandle();
+
   return (
     <StyledContainer>
       {testUserData ? (
@@ -152,71 +193,60 @@ const Profile = () => {
           {
             // the cards section
           }
-          <CardsSection>
-            {showCardHandler(domainCivil) ? (
-              <Domains>
-                <h1 style={ domainTitleStyle }>Civil</h1><br />
-                {showOptionsHandler(domainCivil).map((main) => main)}
-              </Domains>
-            ) : (
-              <></>
-            )}
-            {showCardHandler(domainCoding) ? (
-              <Domains>
-                <h1 style={ domainTitleStyle } >Coding</h1><br />
-                {showOptionsHandler(domainCoding).map((main) => main)}
-              </Domains>
-            ) : (
-              <></>
-            )}
-            {showCardHandler(domainGaming) ? (
-              <Domains>
-                <h1 style={ domainTitleStyle } >Gaming</h1><br />
-                {showOptionsHandler(domainGaming).map((main) => main)}
-              </Domains>
-            ) : (
-              <></>
-            )}
-            {showCardHandler(domainElectrical) ? (
-              <Domains>
-                <h1 style={ domainTitleStyle } >Electrical</h1><br />
-                {showOptionsHandler(domainElectrical).map((main) => main)}
-              </Domains>
-            ) : (
-              <></>
-            )}
-            {showCardHandler(domainGenral) ? (
-              <Domains>
-                <h1 style={ domainTitleStyle } >General</h1><br />
-                {showOptionsHandler(domainGenral).map((main) => main)}
-              </Domains>
-            ) : (
-              <></>
-            )}
-            {showCardHandler(domainRobotics) ? (
-              <Domains>
-                <h1 style={ domainTitleStyle } >Robotics</h1><br />
-                {showOptionsHandler(domainRobotics).map((main) => main)}
-              </Domains>
-            ) : (
-              <></>
-            )}
-            {showCardHandler(domainGenral) ? (
-              <Domains>
-                <h1>Genral</h1>
-                {showOptionsHandler(domainGenral).map((main) => main)}
-              </Domains>
-            ) : (
-              <></>
-            )}
-          </CardsSection>
+         
+            <CardsSection>
+              {showCardHandler(domainCivil) ? (
+                <Domains>
+                  <h1 style={domainTitleStyle}>Civil</h1>
+                  <br />
+                  {showOptionsHandler(domainCivil).map((main) => main)}
+                </Domains>
+              ) : null}
+              {showCardHandler(domainCoding) ? (
+                <Domains>
+                  <h1 style={domainTitleStyle}>Coding</h1>
+                  <br />
+                  {showOptionsHandler(domainCoding).map((main) => main)}
+                </Domains>
+              ) : null}
+              {showCardHandler(domainGaming) ? (
+                <Domains>
+                  <h1 style={domainTitleStyle}>Gaming</h1>
+                  <br />
+                  {showOptionsHandler(domainGaming).map((main) => main)}
+                </Domains>
+              ) : null}
+              {showCardHandler(domainElectrical) ? (
+                <Domains>
+                  <h1 style={domainTitleStyle}>Electrical</h1>
+                  <br />
+                  {showOptionsHandler(domainElectrical).map((main) => main)}
+                </Domains>
+              ) : null}
+              {showCardHandler(domainGenral) ? (
+                <Domains>
+                  <h1 style={domainTitleStyle}>General</h1>
+                  <br />
+                  {showOptionsHandler(domainGenral).map((main) => main)}
+                </Domains>
+              ) : null}
+                 
+              {showCardHandler(domainRobotics) ? (
+                <Domains>
+                  <h1 style={domainTitleStyle}>Robotics</h1>
+                  <br />
+                  {showOptionsHandler(domainRobotics).map((main) => main)}
+                </Domains>
+              ) : null}
+           
+            </CardsSection>
+         
         </>
-
-      )  : <h1 style={{ color: "white" }}>{
-        GID ? "": "You are not Registered !!" }</h1>
-      }
-
-      
+      ) : (
+        <h1 style={{ color: "white" }}>
+          {GID ? "" : "You are not Registered !!"}
+        </h1>
+      )}
     </StyledContainer>
   );
 };
