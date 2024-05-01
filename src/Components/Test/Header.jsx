0de2e6a24582fd29126be84efa82c0cf3f46f3d4
ByteZO <coder.gny@gmail.com>
2024-05-01 18:40:36 +0530

    Domain login update

--- src/Components/Test/Header.jsx
+++ src/Components/Test/Header.jsx
@@ -1,38 +1,22 @@
-import React, { useEffect, useRef } from "react";
-import styled from "styled-components";
-import gsap from "gsap";
-import SplitType from "gsap/SplitText"; // Import SplitType explicitly
-
-gsap.registerPlugin(SplitType); // Register SplitType with GSAP
-
-const Header = styled.header`
-  display: grid;
-  place-content: center;
-  height: 100vh;
-`;
-
-const Heading = styled.h1`
-  font-size: 7rem;
-  text-transform: uppercase;
-  font-family: "Bebas Neue", sans-serif; /* Ensure fallback font */
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
-  line-height: 5.9rem;
-
-  .char {
-    transform: translateY(115px);
-    transition: transform 0.5s;
-  }
-`;
+import React, { useEffect, useRef, useState } from "react";
 
 const HomePage = () => {
-  const textRef = useRef(null);
+  const [inputs, setInputs] = useState([]);
+  const maxInputs = 5;
 
+  const addInput = () => {
+    if (inputs.length < maxInputs) {
+      const inputValue = prompt("Enter a number:");
+      setInputs((prevInputs) => [...prevInputs, inputValue]);
+      console.log(inputValue);
+    }
+  };
 
-
+  console.log("this is the last one :: ", inputs.length, inputs);
   return (
-    <Header>
-      <Heading ref={textRef}>woah.text</Heading>
-    </Header>
+    <>
+      <input style={{ margin: "600px" }} type="text" onChange={addInput} />
+    </>
   );
 };
 
